e962f0803c03041c48f51633d78351f19da9b209
rguindi <raul_fv@hotmail.com>
2024-02-27 00:52:18 +0100

    validado

--- API/Database/db.js
+++ API/Database/db.js
@@ -1,7 +1,7 @@
 const mysql=require('mysql2')
 //CONFIGURACION BASE DE DATOS
 const db= mysql.createPool({
-    host: '0.0.0.0',
+    host: '192.168.1.135',
     port: 3307,
     user: 'inventario',
     password: 'inventario',
